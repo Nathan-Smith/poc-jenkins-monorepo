713cdd95dcc8b3c870d91780296d5673530e0158
Nathan Smith <nathan.a.smith@live.com>
2021-04-23 10:56:18 +1000

    feature: Static Pipeline Generation
    * New build-pipeline-generator tool that traverses the monorepo, detecting components and producing a Jenkinsfile running independent stages in parallel while following component dependencies.
    * Components now can be placed in sub-directories.
    * Components must now have a VERSION file.
    * Components must now have a Jenkinsfile fragment that contains at least 1 stage, each stage can run within a defined agent.
    * Adding VSCode Extension recommendation [Code Spell Checker](https://marketplace.visualstudio.com/items?itemName=streetsidesoftware.code-spell-checker).
    * Adding VSCode Extension recommendations [ESLint](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint), [Prettier](https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode) and [EditorConfig](https://marketplace.visualstudio.com/items?itemName=EditorConfig.EditorConfig) to ensure consistency code styling and prevent common bugs.
    * Adding VSCode Launch Configuration for attaching a Debugger to Node docker containers, that expose the debugger port.

---
+++ cicd/build-pipeline-generator/src/__tests__/gen-pipeline.test.ts
@@ -0,0 +1,203 @@
+import { DepGraph } from 'dependency-graph'
+
+import { Component } from '../gen-dep-graph'
+import generatePipeline from '../gen-pipeline'
+
+test('empty graph', () => {
+  const graph = new DepGraph<Component>()
+
+  const pipeline = generatePipeline(graph)
+  expect(pipeline.parallelStages.length).toBe(0)
+})
+
+test(`
+.
+└── app1
+`, () => {
+  const graph = new DepGraph<Component>()
+
+  graph.addNode('app1', { name: 'app1', path: '' })
+
+  const pipeline = generatePipeline(graph)
+  expect(pipeline.parallelStages.length).toBe(1)
+  expect(pipeline.parallelStages[0].name).toEqual('1')
+  expect(pipeline.parallelStages[0].stages.length).toBe(1)
+  expect(pipeline.parallelStages[0].stages[0].name).toEqual('app1')
+})
+
+test(`
+.
+└── app1
+    └── lib1
+`, () => {
+  const graph = new DepGraph<Component>()
+
+  graph.addNode('app1', { name: 'app1', path: '' })
+  graph.addNode('lib1', { name: 'lib1', path: '' })
+
+  graph.addDependency('app1', 'lib1')
+
+  const pipeline = generatePipeline(graph)
+  expect(pipeline.parallelStages.length).toBe(2)
+  expect(pipeline.parallelStages[0].name).toEqual('1')
+  expect(pipeline.parallelStages[0].stages.length).toBe(1)
+  expect(pipeline.parallelStages[0].stages[0].name).toEqual('lib1')
+  expect(pipeline.parallelStages[1].name).toEqual('2')
+  expect(pipeline.parallelStages[1].stages.length).toBe(1)
+  expect(pipeline.parallelStages[1].stages[0].name).toEqual('app1')
+})
+
+test(`
+.
+├── app1
+│   └── lib1
+└── app2
+    └── lib1
+`, () => {
+  const graph = new DepGraph<Component>()
+
+  graph.addNode('app1', { name: 'app1', path: '' })
+  graph.addNode('app2', { name: 'app2', path: '' })
+  graph.addNode('lib1', { name: 'lib1', path: '' })
+
+  graph.addDependency('app1', 'lib1')
+  graph.addDependency('app2', 'lib1')
+
+  const pipeline = generatePipeline(graph)
+  expect(pipeline.parallelStages.length).toBe(2)
+  expect(pipeline.parallelStages[0].name).toEqual('1')
+  expect(pipeline.parallelStages[0].stages.length).toBe(1)
+  expect(pipeline.parallelStages[0].stages[0].name).toEqual('lib1')
+  expect(pipeline.parallelStages[1].name).toEqual('2')
+  expect(pipeline.parallelStages[1].stages.length).toBe(2)
+  expect(pipeline.parallelStages[1].stages[0].name).toEqual('app1')
+  expect(pipeline.parallelStages[1].stages[1].name).toEqual('app2')
+})
+
+test(`
+.
+├── app1
+│   ├── lib1
+│   └── lib2
+└── app2
+    ├── lib1
+    └── lib2
+`, () => {
+  const graph = new DepGraph<Component>()
+
+  graph.addNode('app1', { name: 'app1', path: '' })
+  graph.addNode('app2', { name: 'app2', path: '' })
+  graph.addNode('lib1', { name: 'lib1', path: '' })
+  graph.addNode('lib2', { name: 'lib2', path: '' })
+
+  graph.addDependency('app1', 'lib1')
+  graph.addDependency('app1', 'lib2')
+  graph.addDependency('app2', 'lib1')
+  graph.addDependency('app2', 'lib2')
+
+  const pipeline = generatePipeline(graph)
+  expect(pipeline.parallelStages.length).toBe(2)
+  expect(pipeline.parallelStages[0].name).toEqual('1')
+  expect(pipeline.parallelStages[0].stages.length).toBe(2)
+  expect(pipeline.parallelStages[0].stages[0].name).toEqual('lib1')
+  expect(pipeline.parallelStages[0].stages[1].name).toEqual('lib2')
+  expect(pipeline.parallelStages[1].name).toEqual('2')
+  expect(pipeline.parallelStages[1].stages.length).toBe(2)
+  expect(pipeline.parallelStages[1].stages[0].name).toEqual('app1')
+  expect(pipeline.parallelStages[1].stages[1].name).toEqual('app2')
+})
+
+test(`
+.
+└── tests1
+    ├── app1
+    │   ├── lib1
+    │   └── lib2
+    └── app2
+        ├── lib1
+        └── lib2
+`, () => {
+  const graph = new DepGraph<Component>()
+
+  graph.addNode('app1', { name: 'app1', path: '' })
+  graph.addNode('app2', { name: 'app2', path: '' })
+  graph.addNode('lib1', { name: 'lib1', path: '' })
+  graph.addNode('lib2', { name: 'lib2', path: '' })
+  graph.addNode('tests1', { name: 'tests1', path: '' })
+
+  graph.addDependency('app1', 'lib1')
+  graph.addDependency('app1', 'lib2')
+  graph.addDependency('app2', 'lib1')
+  graph.addDependency('app2', 'lib2')
+  graph.addDependency('tests1', 'app1')
+  graph.addDependency('tests1', 'app2')
+
+  const pipeline = generatePipeline(graph)
+  expect(pipeline.parallelStages.length).toBe(3)
+  expect(pipeline.parallelStages[0].name).toEqual('1')
+  expect(pipeline.parallelStages[0].stages.length).toBe(2)
+  expect(pipeline.parallelStages[0].stages[0].name).toEqual('lib1')
+  expect(pipeline.parallelStages[0].stages[1].name).toEqual('lib2')
+  expect(pipeline.parallelStages[1].name).toEqual('2')
+  expect(pipeline.parallelStages[1].stages.length).toBe(2)
+  expect(pipeline.parallelStages[1].stages[0].name).toEqual('app1')
+  expect(pipeline.parallelStages[1].stages[1].name).toEqual('app2')
+  expect(pipeline.parallelStages[2].name).toEqual('3')
+  expect(pipeline.parallelStages[2].stages.length).toBe(1)
+  expect(pipeline.parallelStages[2].stages[0].name).toEqual('tests1')
+})
+
+test(`
+.
+├── app3
+├── tests1
+│   ├── app1
+│   │   ├── lib1
+│   │   └── lib3
+│   │       └── lib2
+│   └── app2
+│       └── lib3
+│           └── lib2
+└── tests2
+    └── app2
+        └── lib3
+            └── lib2
+`, () => {
+  const graph = new DepGraph<Component>()
+
+  graph.addNode('app1', { name: 'app1', path: '' })
+  graph.addNode('app2', { name: 'app2', path: '' })
+  graph.addNode('app3', { name: 'app3', path: '' })
+  graph.addNode('lib1', { name: 'lib1', path: '' })
+  graph.addNode('lib2', { name: 'lib2', path: '' })
+  graph.addNode('lib3', { name: 'lib3', path: '' })
+  graph.addNode('tests1', { name: 'tests1', path: '' })
+  graph.addNode('tests2', { name: 'tests2', path: '' })
+
+  graph.addDependency('tests1', 'app1')
+  graph.addDependency('tests1', 'app2')
+  graph.addDependency('tests2', 'app2')
+  graph.addDependency('app1', 'lib1')
+  graph.addDependency('app1', 'lib3')
+  graph.addDependency('app2', 'lib3')
+  graph.addDependency('lib3', 'lib2')
+
+  const pipeline = generatePipeline(graph)
+  expect(pipeline.parallelStages.length).toBe(4)
+  expect(pipeline.parallelStages[0].name).toEqual('1')
+  expect(pipeline.parallelStages[0].stages.length).toBe(3)
+  expect(pipeline.parallelStages[0].stages[0].name).toEqual('app3')
+  expect(pipeline.parallelStages[0].stages[1].name).toEqual('lib1')
+  expect(pipeline.parallelStages[0].stages[2].name).toEqual('lib2')
+  expect(pipeline.parallelStages[1].name).toEqual('2')
+  expect(pipeline.parallelStages[1].stages.length).toBe(1)
+  expect(pipeline.parallelStages[1].stages[0].name).toEqual('lib3')
+  expect(pipeline.parallelStages[2].name).toEqual('3')
+  expect(pipeline.parallelStages[2].stages.length).toBe(2)
+  expect(pipeline.parallelStages[2].stages[0].name).toEqual('app1')
+  expect(pipeline.parallelStages[2].stages[1].name).toEqual('app2')
+  expect(pipeline.parallelStages[3].name).toEqual('4')
+  expect(pipeline.parallelStages[3].stages.length).toBe(2)
+  expect(pipeline.parallelStages[3].stages[0].name).toEqual('tests1')
+  expect(pipeline.parallelStages[3].stages[1].name).toEqual('tests2')
+})
---
+++ cicd/build-pipeline-generator/src/gen-pipeline.ts
@@ -0,0 +1,46 @@
+import { DepGraph } from 'dependency-graph'
+
+import { Component } from './gen-dep-graph'
+
+export interface ParallelStage {
+  name: string
+  stages: Component[]
+}
+
+export interface Pipeline {
+  parallelStages: ParallelStage[]
+}
+
+/**
+ * Converts a Directed Graph into a Build Pipeline that adheres to dependency ordering and optimise performance by grouping unrelated components into stages that can run in parallel
+ * @param graph Directed Graph contains components as nodes and its dependencies as edges
+ * @returns Build Pipeline with dependency ordering and parallelism
+ */
+export default function generatePipeline(graph: DepGraph<Component>): Pipeline {
+  // Exit if nothing to do
+  if (graph.size() === 0) {
+    return {
+      parallelStages: [],
+    }
+  }
+
+  return {
+    parallelStages: [
+      {
+        // Map over all the 'leaves' (components that have no dependencies) combining them into a group to be run in parallel
+        stages: graph.overallOrder(true).map((node) => {
+          const component = graph.getNodeData(node)
+          graph.removeNode(node) // Once the node has been added to the build pipeline, discard it. This leaves components that once had a dependency (this one) now added to the next parallel stage
+          return component
+        }),
+        name: '', // Create the name later once all the parallel stages are known
+      },
+    ]
+      .concat(generatePipeline(graph).parallelStages) // Go down the next level of `leaves` and generate the next parallel stage, this recursively happens until the graph is empty (all components added to the build pipeline), add the resulting parallel stages
+      // Create friendly names for the parallel stages, using its index as a name
+      .map((val, i) => ({
+        ...val,
+        name: `${i + 1}`,
+      })),
+  }
+}
