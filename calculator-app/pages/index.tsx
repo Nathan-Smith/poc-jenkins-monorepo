dc0e1290b02726a0cf96c1d59cdd06db5eee4cdd
Nathan Smith <nathan.a.smith@live.com>
2021-07-29 22:50:01 +1000

    text field wip

--- calculator-app/pages/index.tsx
+++ calculator-app/pages/index.tsx
@@ -2,6 +2,28 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 import { KeypadButton } from '../components/KeypadButton'
 import { KeypadEqual } from '../components/KeypadEqual'
+import { useEffect, useRef } from 'react'
+import { MDCTextField } from '@material/textfield'
+
+export function Result(): JSX.Element {
+  const labelRef = useRef<HTMLLabelElement>(null)
+
+  useEffect(() => {
+    if (labelRef.current) new MDCTextField(labelRef.current)
+  }, [labelRef])
+
+  return (
+    <label className={`mdc-text-field mdc-text-field--filled mdc-text-field--end-aligned ${styles.result}`} style={{gridArea: 'result'}} ref={labelRef}>
+      <span className="mdc-text-field__ripple"></span>
+      <input
+        className="mdc-text-field__input"
+        type="text"
+        aria-labelledby="my-label-id"
+      />
+      <span className="mdc-line-ripple"></span>
+    </label>
+  )
+}
 
 export default function Home(): JSX.Element {
   return (
@@ -13,7 +35,7 @@ export default function Home(): JSX.Element {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.result}>42</div>
+        <Result />
         <KeypadButton gridArea="keypad-1">1</KeypadButton>
         <KeypadButton gridArea="keypad-2">2</KeypadButton>
         <KeypadButton gridArea="keypad-3">3</KeypadButton>
