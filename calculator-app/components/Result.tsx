8031bc8f281a8bd186ec41c3644052bbe2f27532
Nathan Smith <nathan.a.smith@live.com>
2021-08-01 23:30:31 +1000

    remove textfield

--- calculator-app/components/Result.tsx
+++ calculator-app/components/Result.tsx
@@ -1,36 +1,5 @@
 import styles from '../styles/Home.module.scss'
-import { useEffect, useRef, useState } from 'react'
-import { MDCTextField } from '@material/textfield'
 
 export function Result(): JSX.Element {
-  const labelRef = useRef<HTMLLabelElement>(null)
-  const [value, setValue] = useState('')
-
-  useEffect(() => {
-    if (labelRef.current) new MDCTextField(labelRef.current)
-  }, [labelRef])
-
-  // Overkill number validation (/^(-?)(\d(?:\d{0,3}(?<!,),(?:\d{1,3})?)+|\d*)(?<=,\d{3}|[^,]?)((?:\.\d*))?.*/gm
-
-  return (
-    <label
-      className={`mdc-text-field mdc-text-field--filled mdc-text-field--end-aligned ${styles.result}`}
-      style={{ gridArea: 'result' }}
-      ref={labelRef}
-    >
-      <span className="mdc-text-field__ripple"></span>
-      <input
-        data-testid="result-input"
-        className={`mdc-text-field__input ${styles.resultInput}`}
-        type="text"
-        value={value}
-        onChange={(e) => {
-          const [_, sign = '', whole = '', decimal = '', fractional = ''] =
-            e.target.value.match(/^(-?)(\d*)((?:\.\d*)?).*/) ?? []
-          setValue(`${sign}${whole}${decimal}${fractional}`)
-        }}
-      />
-      <span className="mdc-line-ripple"></span>
-    </label>
-  )
+  return <div className={` ${styles.result}`}>42</div>
 }
--- calculator-app/components/__tests__/Result.test.tsx
+++ calculator-app/components/__tests__/Result.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import renderer, { act, ReactTestRenderer } from 'react-test-renderer'
-import { cleanup, fireEvent, render } from '@testing-library/react'
 
 import { Result } from '../Result'
 
@@ -12,38 +11,5 @@ describe('Result', () => {
     })
 
     expect(component?.toJSON()).toMatchSnapshot()
-  })
-
-  test.each`
-    input                 | expected
-    ${'42'}               | ${'42'}
-    ${'test'}             | ${''}
-    ${'t3st'}             | ${''}
-    ${'-42'}              | ${'-42'}
-    ${'42.5646'}          | ${'42.5646'}
-    ${'-42.5646'}         | ${'-42.5646'}
-    ${'-4,242.5646'}      | ${'-4'}
-    ${'-42,424,242.5646'} | ${'-42'}
-    ${'42...'}            | ${'42.'}
-    ${'----42'}           | ${'-'}
-    ${'42,,,,'}           | ${'42'}
-    ${'42,,,,42'}         | ${'42'}
-    ${'424242,424242,'}   | ${'424242'}
-    ${'424,2424242'}      | ${'424'}
-    ${',42424242'}        | ${''}
-    ${'-4,24'}            | ${'-4'}
-    ${'-4,24.242'}        | ${'-4'}
-    ${'42,'}              | ${'42'}
-    ${'42.'}              | ${'42.'}
-  `('"$input" entered should be "$expected"', ({ input, expected }) => {
-    const { getByTestId } = render(<Result />)
-
-    expect((getByTestId(/result-input/i) as HTMLInputElement).value).toBe('')
-
-    fireEvent.change(getByTestId(/result-input/i), { target: { value: input } })
-
-    expect((getByTestId(/result-input/i) as HTMLInputElement).value).toBe(
-      expected
-    )
   })
 })
