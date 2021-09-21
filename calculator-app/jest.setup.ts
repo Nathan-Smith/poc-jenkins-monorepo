eec533a2de2ee5cb85e44855c20cad29c3938ac2
Nathan Smith <nathan.a.smith@live.com>
2021-09-22 00:15:05 +1000

    wip

--- calculator-app/jest.setup.ts
+++ calculator-app/jest.setup.ts
@@ -1,6 +1,5 @@
 import fetch from 'node-fetch'
-// https://www.npmjs.com/package/jest-fetch-mock
-module.exports = async () => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (global.fetch as any) = fetch
-}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(global.fetch as any) = fetch
+
+export default undefined
